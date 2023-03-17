85e668c5dc9e22d0de4b3fa47e633cbee5768226
Valentina Acosta <124893622+ValentinaAcosta@users.noreply.github.com>
2023-03-17 14:07:10 -0500

    Hasta proceso 2FA en el login

---
+++ src/controllers/usuario.controller.ts
@@ -0,0 +1,196 @@
+import {service} from '@loopback/core';
+import {
+  Count,
+  CountSchema,
+  Filter,
+  FilterExcludingWhere,
+  repository,
+  Where
+} from '@loopback/repository';
+import {
+  del, get,
+  getModelSchemaRef, HttpErrors, param, patch, post, put, requestBody,
+  response
+} from '@loopback/rest';
+import {Credenciales, Login, Usuario} from '../models';
+import {LoginRepository, UsuarioRepository} from '../repositories';
+import {SeguridadUsuarioService} from '../services';
+
+export class UsuarioController {
+  constructor(
+    @repository(UsuarioRepository)
+    public usuarioRepository: UsuarioRepository,
+    @service(SeguridadUsuarioService)
+    public servicioSeguridad: SeguridadUsuarioService,
+    @repository(LoginRepository)
+    public repositoryLogin: LoginRepository
+  ) { }
+
+  @post('/usuario')
+  @response(200, {
+    description: 'Usuario model instance',
+    content: {'application/json': {schema: getModelSchemaRef(Usuario)}},
+  })
+  async create(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Usuario, {
+            title: 'NewUsuario',
+            exclude: ['_id'],
+          }),
+        },
+      },
+    })
+    usuario: Omit<Usuario, '_id'>,
+  ): Promise<Usuario> {
+    //Crear la clave
+    let clave = this.servicioSeguridad.crearTextoAleatorio(10);
+    console.log("clave", clave);
+    //Cifrar la clave
+    let claveCifrada = this.servicioSeguridad.cifrarTexto(clave);
+    //Asignar la clave cifrada al usuario
+    usuario.clave = claveCifrada;
+    //Enviar correo electronico de notificacion
+    return this.usuarioRepository.create(usuario);
+  }
+
+  @get('/usuario/count')
+  @response(200, {
+    description: 'Usuario model count',
+    content: {'application/json': {schema: CountSchema}},
+  })
+  async count(
+    @param.where(Usuario) where?: Where<Usuario>,
+  ): Promise<Count> {
+    return this.usuarioRepository.count(where);
+  }
+
+  @get('/usuario')
+  @response(200, {
+    description: 'Array of Usuario model instances',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'array',
+          items: getModelSchemaRef(Usuario, {includeRelations: true}),
+        },
+      },
+    },
+  })
+  async find(
+    @param.filter(Usuario) filter?: Filter<Usuario>,
+  ): Promise<Usuario[]> {
+    return this.usuarioRepository.find(filter);
+  }
+
+  @patch('/usuario')
+  @response(200, {
+    description: 'Usuario PATCH success count',
+    content: {'application/json': {schema: CountSchema}},
+  })
+  async updateAll(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Usuario, {partial: true}),
+        },
+      },
+    })
+    usuario: Usuario,
+    @param.where(Usuario) where?: Where<Usuario>,
+  ): Promise<Count> {
+    return this.usuarioRepository.updateAll(usuario, where);
+  }
+
+  @get('/usuario/{id}')
+  @response(200, {
+    description: 'Usuario model instance',
+    content: {
+      'application/json': {
+        schema: getModelSchemaRef(Usuario, {includeRelations: true}),
+      },
+    },
+  })
+  async findById(
+    @param.path.string('id') id: string,
+    @param.filter(Usuario, {exclude: 'where'}) filter?: FilterExcludingWhere<Usuario>
+  ): Promise<Usuario> {
+    return this.usuarioRepository.findById(id, filter);
+  }
+
+  @patch('/usuario/{id}')
+  @response(204, {
+    description: 'Usuario PATCH success',
+  })
+  async updateById(
+    @param.path.string('id') id: string,
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Usuario, {partial: true}),
+        },
+      },
+    })
+    usuario: Usuario,
+  ): Promise<void> {
+    await this.usuarioRepository.updateById(id, usuario);
+  }
+
+  @put('/usuario/{id}')
+  @response(204, {
+    description: 'Usuario PUT success',
+  })
+  async replaceById(
+    @param.path.string('id') id: string,
+    @requestBody() usuario: Usuario,
+  ): Promise<void> {
+    await this.usuarioRepository.replaceById(id, usuario);
+  }
+
+  @del('/usuario/{id}')
+  @response(204, {
+    description: 'Usuario DELETE success',
+  })
+  async deleteById(@param.path.string('id') id: string): Promise<void> {
+    await this.usuarioRepository.deleteById(id);
+  }
+
+  /**
+   * Métodos personalizados para la API
+   */
+
+  @post('identificar-usuario')
+  @response(200, {
+    description: "Identificar un usuario por correo y clave",
+    content: {'application/json': {schema: getModelSchemaRef(Credenciales)}}
+  })
+  async identificarUsuario(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Credenciales)
+        }
+      }
+    }
+
+    )
+    credenciales: Credenciales
+  ): Promise<object> {
+    let usuario = await this.servicioSeguridad.identificarUsuario(credenciales);
+    if (usuario) {
+      let codigo2fa = this.servicioSeguridad.crearTextoAleatorio(5);
+      let login: Login = new Login();
+      login.usuarioId = usuario._id!;
+      login.codigo2fa = codigo2fa;
+      login.estadoCodigo2fa = false;
+      login.token = "";
+      login.estadoToken = false;
+      this.repositoryLogin.create(login);
+      //notificar al usuario via correo o sms
+      return usuario
+    }
+    return new HttpErrors[401]("Credenciales incorrectas.");
+  }
+
+}
---
+++ src/services/seguridad-usuario.service.ts
@@ -0,0 +1,54 @@
+import { /* inject, */ BindingScope, injectable} from '@loopback/core';
+import {repository} from '@loopback/repository';
+import {Credenciales, Usuario} from '../models';
+import {UsuarioRepository} from '../repositories';
+const generator = require('generate-password');
+const MD5 = require("crypto-js/md5");
+
+@injectable({scope: BindingScope.TRANSIENT})
+export class SeguridadUsuarioService {
+  constructor(
+    @repository(UsuarioRepository)
+    public repositorioUsuario: UsuarioRepository
+  ) { }
+
+  /**
+   * Crear una clave aleatoria
+   * @returns cadena aleatoria de n caracteres
+   */
+  crearTextoAleatorio(n: number): string {
+    let clave = generator.generate({
+      length: n,
+      numbers: true
+    });
+    return clave;
+
+  }
+
+  /**
+   * Cifrar una cadena con metodo md5
+   * @param cadena texto a cifrar
+   * @returns cadena cifrada con md5
+   */
+
+  cifrarTexto(cadena: string): string {
+    let cadenaCifrada = MD5(cadena).toString();
+    return cadenaCifrada;
+  }
+
+  /**
+   * Se busca un usuario por sus credenciales de acceso
+   * @param credenciales credenciales del usuario
+   * @returns usuario encontrado o nulo
+   */
+  async identificarUsuario(credenciales: Credenciales): Promise<Usuario | null> {
+    let usuario = await this.repositorioUsuario.findOne({
+      where: {
+        correo: credenciales.correo,
+        clave: credenciales.clave
+      }
+    });
+    return usuario as Usuario;
+
+  }
+}
